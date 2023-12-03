d792b270df7a9a56f2b2a2ad220d3e5652b861dc
lokmanhaakim <lokmanhaakimm@gmail.com>
2023-12-04 07:19:28 +0800

    Radian to degree formula

--- getCoordinates.m
+++ getCoordinates.m
@@ -5,7 +5,10 @@ lat1 = deg2rad(lat1);
 lon1 = deg2rad(lon1);
 a = deg2rad(bearing);
 
-lat2 = asin(sin(lat1)*cos(d/R)+(cos(lat1))*(sin(d/R))*cos(a));
+lat2 = asin(sin(lat1)*cos(d/R)+(cos(lat1))*(sin(d/R)*cos(a)));
 lon2 = lon1+atan2(sin(a) * sin(d/R) * cos(lat1),cos(d/R) - sin(lat1) * sin(lat2));
+
+lat2 = rad2deg(lat2);
+lon2 = rad2deg(lon2);
 
 end
