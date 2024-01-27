ddcb5ddea4c3f2167a94ad0bfd81297a928ec8ae
lokmanhaakim <lokmanhaakimm@gmail.com>
2024-01-27 14:51:53 +0800

    Add draft file

---
+++ test.py
@@ -0,0 +1,22 @@
+# %%
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/python-machine-learning-book-2nd-edition/master/code/ch10/housing.data.txt',header=None,sep='\s+')
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS',
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+# %%
+df2 = pd.read_csv('https://raw.githubusercontent.com/lokmanhaakim/study/main/lookup_item.csv',header=0,sep=',')
+df2= df2.dropna()
+print(df2['item_category'])
+
+#%%
+# If not already installed, do: pip install pandas fastparquet
+import pandas as pd
+
+URL_DATA = 'https://storage.dosm.gov.my/population/population_district.parquet'
+
+df = pd.read_parquet(URL_DATA)
+if 'date' in df.columns: df['date'] = pd.to_datetime(df['date'])
+
+print(df)
+# %%
