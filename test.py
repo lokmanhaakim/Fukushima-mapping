0c55840f111af21d259e7f39c30240fafb5db98a
lokmanhaakim <lokmanhaakimm@gmail.com>
2024-01-27 14:48:18 +0800

    edit file

--- test.py
+++ test.py
@@ -1,86 +1,22 @@
-import cv2
-from cvzone.HandTrackingModule import HandDetector
-from cvzone.ClassificationModule import Classifier
-import numpy as np
-import math
-import tensorflow
-import time
+# %%
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/python-machine-learning-book-2nd-edition/master/code/ch10/housing.data.txt',header=None,sep='\s+')
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS',
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+# %%
+df2 = pd.read_csv('https://raw.githubusercontent.com/lokmanhaakim/study/main/lookup_item.csv',header=0,sep=',')
+df2= df2.dropna()
+print(df2['item_category'])
 
-letter = ""
-alphabet = ['U','O','Y']
+#%%
+# If not already installed, do: pip install pandas fastparquet
+import pandas as pd
 
-cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
-classfier = Classifier("Imagemodel/keras_model.h5","Imagemodel/labels.txt")
-offset = 20
-imgSize = 300
-folder = "Data/Y"
-counter = 0
+URL_DATA = 'https://storage.dosm.gov.my/population/population_district.parquet'
 
-labels = {'O','U','Y'}
+df = pd.read_parquet(URL_DATA)
+if 'date' in df.columns: df['date'] = pd.to_datetime(df['date'])
 
-
-while True:
-    success, img = cap.read()
-    hands, img = detector.findHands(img)
-    if hands:
-        hand = hands[0]
-        x, y, w, h = hand['bbox']
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
-
-        imgCrop = img[y - offset: y + h + offset, x:x + w + offset]
-        imgCropShape = imgCrop.shape
-        # cv2.imshow("ImageCrop", imgCrop)
-
-        aspectRatio = h / w
-
-        if aspectRatio > 1:
-            k = imgSize / h
-            wCal = math.ceil(k*w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            imgResizeShape = imgResize.shape
-            wGap = math.ceil((imgSize - wCal)/2)
-            imgWhite[:, wGap:wCal+wGap] = imgResize
-            prediction, index = Classifier.getPrediction(imgWhite)
-            print(prediction,index)
-            time.sleep(2)
-            for i in range (0,len(prediction)):
-                if (prediction[i]> 0.98) :
-                    letter += alphabet[index]
-            print(letter)
-        else:
-            k = imgSize / w
-            hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imgSize,hCal))
-            imgResizeShape = imgResize.shape
-            hGap = math.ceil((imgSize- hCal) / 2)
-            imgWhite[hGap:hCal + hGap, :] = imgResize
-            # prediction, index = Classifier.getPrediction(imgWhite, draw=False)
-        # cv2.rectangle(imgOutput, (x - offset, y - offset - 50),(x - offset + 90, y - offset - 50 + 50), (255, 0, 255), cv2.FILLED)
-        # cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
-        # cv2.rectangle(imgOutput, (x - offset, y - offset),(x + w + offset, y + h + offset), (255, 0, 255), 4)
-
-        # cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite2", imgWhite)
-
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
-
-    # if key == ord("e"):
-    #     print(letter)
-    #     break
-
-
-
-    # if key == ord("e"):
-    #     break
-
-        # import os
-        # root_path = r'C:\Users\USER\PycharmProjects\pythonProject\Data'
-        #
-        # list = ['O','P','Q','R','S','T','U','V','W','X','Y','Z']
-        #
-        # for items in list:
-        #     path = os.path.join(root_path, items)
-        #     os.mkdir(path)
+print(df)
+# %%
